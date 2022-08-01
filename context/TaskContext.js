025542300d71850f206632e796392565972b9e48
Gökmen Özkan <gkmenozkn@gmail.com>
2022-08-01 16:53:27 +0300

    database update for authenticated user

--- context/TaskContext.js
+++ context/TaskContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { db } from '../firebase/firebaseConfig';
 import { collection, onSnapshot } from 'firebase/firestore';
+import { useAuthContext } from './AuthContext';
 
 const Context = createContext();
 
@@ -8,17 +9,35 @@ export const useTaskContext = () => useContext(Context);
 
 export default function TaskProvider({ children }) {
   const [tasks, setTasks] = useState([]);
+  const { authUser } = useAuthContext();
 
   useEffect(() => {
-    onSnapshot(collection(db, 'tasks'), (snapShot) => {
-      setTasks(
-        snapShot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }))
-      );
-    });
+    if (authUser) {
+      onSnapshot(collection(db, 'tasks'), (snapShot) => {
+        setTasks(
+          snapShot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+          );
+        });
+      }
+  /* eslint-disable-next-line */
   }, []);
+  
+  useEffect(() => {
+    if (authUser) {
+      onSnapshot(collection(db, 'tasks'), (snapShot) => {
+        setTasks(
+          snapShot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+          );
+        });
+      }
+  /* eslint-disable-next-line */
+  }, [authUser]);
 
   const value = {
     tasks,
